0f351a489ecc8fcf7b1fb1eea4d7d77d958aa05a
Daniele Di Palma <daniele.dipalma68@gmail.com>
2021-08-29 11:21:13 +0200

    DFA che accetta il linguaggio delle stringhe dove 'a' occorre almeno una volta tra le ultime 3 posizioni della stringa

---
+++ 1. DFA/E7_UltimeTrePos.java
@@ -0,0 +1,71 @@
+/**
+ * Esercitazione 1.7 LFT 
+ * @author Daniele Di Palma
+ * 
+ * Automa per riconoscere il linguaggio delle stringhe tali che "a" occorre 
+ * almeno una volta in una delle ultime tre posizioni della stringa
+ * 
+ * Il DFA accetta anche stringhe < 3 simboli dove almeno un simbolo è "a"
+ * 
+ * Esempi di stringhe accettate: "ba", "abb", "a", "aa", "bba"
+ * 
+ * Esempi stringhe non accettate: "bbabbbb", "b", "abbb"
+ */
+
+public class E7_UltimeTrePos {
+    
+    public static boolean scan(String s){
+
+        int state = 0;
+        int i = 0;
+
+        while(state >= 0 && i < s.length()){
+
+            final char ch = s.charAt(i++);
+
+            switch(state){
+                case 0:
+                    if(ch == 'a')
+                        state = 1;
+                    else if(ch == 'b')
+                        state = 0;
+                    else
+                        state = -1;
+                    break;
+                
+                case 1:
+                    if(ch == 'a')
+                        state = 1;
+                    else if(ch == 'b')
+                        state = 2;
+                    else
+                        state = -1;
+                    break;
+
+                case 2:
+                    if(ch == 'a')
+                        state = 1;
+                    else if(ch == 'b')
+                        state = 3;
+                    else
+                        state = -1;
+                    break;
+
+                case 3:
+                    if(ch == 'a')
+                        state = 1;
+                    else if(ch == 'b')
+                        state = 0;
+                    else
+                        state = -1;
+                    break;
+            }
+        }
+
+        return state == 1 || state == 2 || state == 3;
+    }
+
+    public static void main(String[] args){
+        System.out.println(scan(args[0]) ? "OK" : "NOPE");
+    }
+}
